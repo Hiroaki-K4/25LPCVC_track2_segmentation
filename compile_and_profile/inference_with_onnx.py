d3b0d540b6fcc53f6a262ce1d1559dc8208c558e
Hiroaki-K4 <49no19@gmail.com>
2025-03-22 16:59:58 -0700

    Add sam2 onnx to inference

---
+++ compile_and_profile/inference_with_onnx.py
@@ -0,0 +1,104 @@
+import json
+from pathlib import Path
+from typing import Optional
+
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from compile_profile_inference_aihub import prepare_data
+from models import GroundedSAMOnnx, GroundingDino
+
+
+def main():
+    # Prepare input data
+    data = load_data()
+
+    # Inference
+    inference(data)
+
+
+def load_data(image_id: Optional[int] = None):
+    image_dir = Path('./compile_and_profile/images/default')
+    annotation_path = Path('./compile_and_profile/annotations/instances_default.json')
+    with annotation_path.open() as f:
+        annotations = json.load(f)
+
+    data = list()
+    for annotation in tqdm(annotations['annotations']):
+        if image_id is not None and annotation['image_id'] != image_id:
+            continue
+        for image in annotations['images']:
+            if image['id'] == annotation['image_id']:
+                break
+        else:
+            raise Exception("image not found")
+        image_path = image_dir / image['file_name']
+        texts = [v for v in annotation['attributes'].values() if isinstance(v, str) and len(v) > 0]
+        for text in texts:
+            image_input, text_input = prepare_data(image_path, text)
+            data.append({
+                'image_name': image_path.name,
+                'annotation_id': annotation['id'],
+                'text': text,
+                'image_input': image_input,
+                'text_input': text_input
+            })
+    return data
+
+
+def inference(data):
+    GROUNDING_DINO_CONFIG_PATH = "./configs/groundingdino/GroundingDINO_SwinT_OGC.py"
+    GROUNDING_DINO_CHECKPOINT_PATH = "./lpcvc_track2_models/groundingdino_swint_ogc.pth"
+    grounding_dino_model = GroundingDino(model_config_path=GROUNDING_DINO_CONFIG_PATH, model_checkpoint_path=GROUNDING_DINO_CHECKPOINT_PATH)
+
+    sam2_encoder_model_path = "lpcvc_track2_models/sam2.1_hiera_base_plus_encoder.onnx"
+    sam2_decoder_model_path = "lpcvc_track2_models/decoder.onnx"
+
+    model = GroundedSAMOnnx(grounding_dino_model, sam2_encoder_model_path, sam2_decoder_model_path)
+
+    # Inference
+    # onnx_path = Path("./compile_and_profile/onnx")
+    # output_path = onnx_path / "output"
+    torch_path = Path("./compile_and_profile/torch")
+    output_path = torch_path / "output"
+    numpy_path = output_path / 'numpy'
+    images_path = output_path / 'images'
+    numpy_path.mkdir(parents=True, exist_ok=True)
+    images_path.mkdir(parents=True, exist_ok=True)
+    device = torch.device("cpu")
+    # providers = ["CPUExecutionProvider"]
+
+    # session = onnxruntime.InferenceSession(
+    #     f"{onnx_path}/model.onnx", providers=providers
+    # )
+    # for d in tqdm(data):
+    #     inputs = {
+    #         "image_input": d['image_input'].detach().cpu().numpy(),
+    #         "text_input": d['text_input'].detach().cpu().numpy(),
+    #     }
+    #     outputs = session.run(None, inputs)
+    #     # save output
+    #     np.save(numpy_path / f"{d['image_name'].replace(' ', '_')}_{d['annotation_id']}_{d['text'].replace(' ', '_')}.npy", outputs[0])
+
+    #     # Save visualization of model output
+    #     plt.matshow(outputs[0])
+    #     plt.axis("off")
+    #     plt.savefig(images_path / f"{d['image_name'].replace(' ', '_')}_{d['annotation_id']}_{d['text'].replace(' ', '_')}.jpg")
+    for d in tqdm(data):
+        unique_fp = f"{d['image_name'].replace(' ', '_')}_{d['annotation_id']}_{d['text'].replace(' ', '_')}"
+        image_save_dir = Path('./compile_and_profile') / 'annotated' / unique_fp
+        d["image_input"] = d["image_input"].to(device)  # Move inputs to same device
+        d["text_input"] = d["text_input"].to(device)
+        output = model(d['image_input'], d['text_input'], image_save_dir)
+        # save output
+        np.save(numpy_path / f"{unique_fp}.npy", output)
+
+        # Save visualization of model output
+        plt.matshow(output)
+        plt.axis("off")
+        plt.savefig(images_path / f"{unique_fp}.jpg")
+
+
+if __name__ == '__main__':
+    main()
